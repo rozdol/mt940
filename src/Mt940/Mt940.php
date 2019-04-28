c40def9e347ebb1ee9f3b819f10007fa7974b34f
Alexander Titov <rozdol@gmail.com>
2019-04-28 08:06:55 +0200

    reversed transactions, types

--- src/Mt940/Mt940.php
+++ src/Mt940/Mt940.php
@@ -281,6 +281,65 @@ class Mt940
 
     public function transaction(array $lines, $openingBalanceDate='',$closingBalanceDate='')
     {
+        $codes=[
+            'BNK'=>'Securities Related Item – Bank fees',
+            'BOE'=>'Bill of exchange',
+            'BRF'=>'Brokerage fee',
+            'CAR'=>'Securities Related Item – Corporate Actions Related (Should only be used when no specific corporate action event code is available)',
+            'CAS'=>'Securities Related Item – Cash in Lieu',
+            'CHG'=>'Charges and other expenses',
+            'CHK'=>'Cheques',
+            'CLR'=>'Cash letters/Cheques remittance',
+            'CMI'=>'Cash management item – No detail',
+            'CMN'=>'Cash management item – Notional pooling',
+            'CMP'=>'Compensation claims',
+            'CMS'=>'Cash management item – Sweeping',
+            'CMT'=>'Cash management item -Topping',
+            'CMZ'=>'Cash management item – Zero balancing',
+            'COL'=>'Collections (used when entering a principal amount)',
+            'COM'=>'Commission',
+            'CPN'=>'Securities Related Item – Coupon payments',
+            'DCR'=>'Documentary credit (used when entering a principal amount)',
+            'DDT'=>'Direct Debit Item',
+            'DIS'=>'Securities Related Item – Gains disbursement',
+            'DIV'=>'Securities Related Item – Dividends',
+            'EQA'=>'Equivalent amount',
+            'EXT'=>'Securities Related Item – External transfer for own account',
+            'FEX'=>'Foreign exchange',
+            'INT'=>'Interest',
+            'LBX'=>'Lock box',
+            'LDP'=>'Loan deposit',
+            'MAR'=>'Securities Related Item – Margin payments/Receipts',
+            'MAT'=>'Securities Related Item – Maturity',
+            'MGT'=>'Securities Related Item – Management fees',
+            'MSC'=>'Miscellaneous',
+            'NWI'=>'Securities Related Item – New issues distribution',
+            'ODC'=>'Overdraft charge',
+            'OPT'=>'Securities Related Item – Options',
+            'PCH'=>'Securities Related Item – Purchase (including STIF and Time deposits)',
+            'POP'=>'Securities Related Item – Pair-off proceeds',
+            'PRN'=>'Securities Related Item – Principal pay-down/pay-up',
+            'REC'=>'Securities Related Item – Tax reclaim',
+            'RED'=>'Securities Related Item – Redemption/Withdrawal',
+            'RIG'=>'Securities Related Item – Rights',
+            'RTI'=>'Returned item',
+            'SAL'=>'Securities Related Item – Sale (including STIF and Time deposits)',
+            'SEC'=>'Securities (used when entering a principal amount)',
+            'SLE'=>'Securities Related Item – Securities lending related',
+            'STO'=>'Standing order',
+            'STP'=>'Securities Related Item – Stamp duty',
+            'SUB'=>'Securities Related Item – Subscription',
+            'SWP'=>'Securities Related Item – SWAP payment',
+            'TAX'=>'Securities Related Item – Withholding tax payment',
+            'TCK'=>'Travellers cheques',
+            'TCM'=>'Securities Related Item – Tripartite collateral management',
+            'TRA'=>'Securities Related Item – Internal transfer for own account',
+            'TRF'=>'Transfer',
+            'TRN'=>'Securities Related Item – Transaction fee',
+            'UWC'=>'Securities Related Item – Underwriting commission',
+            'VDA'=>'Value date adjustment (used with an entry made to withdraw an incorrectly dated entry – it will be followed by the correct entry with the relevant code)',
+            'WAR'=>'Securities Related Item – Warrant'
+        ];
         $lines[0]=str_ireplace("\r\n","//",$lines[0]);
         //$lines[0]=str_ireplace("\r","//",$lines[0]);
         //echo $this->html->pre_display($lines,"lines");
@@ -294,18 +353,41 @@ class Mt940
         }
 
 
+        $type=substr($match[8],-3);
+        $type_descr=$codes[$type];
+        //echo $this->html->pre_display($match,"match $type - $type_descr");
         // Parse the amount
         $amount = (float) str_replace(',', '.', $match[7]);
-        if ($match[5] === 'D' || $match[5] === 'ED' || $match[5] === 'RD') {
+        if ($match[5] === 'D') {
+            $dr=$amount;
+            $cr=0;
             $amount *= -1;
         }
-        if($amount>0){
-            $dr=0;
-            $cr=$amount;
-        }else{
+        if ($match[5] === 'ED' || $match[5] === 'RD') {
             $dr=-1*$amount;
             $cr=0;
+            //$amount *= -1;
         }
+
+        if ($match[5] === 'C') {
+            $dr=0;
+            $cr=$amount;
+            //$amount *= -1;
+        }
+        if ($match[5] === 'EC' || $match[5] === 'RC') {
+            $dr=0;
+            $cr=-1*$amount;
+            $amount *= -1;
+        }
+
+        // if($amount>0){
+        //     $dr=0;
+        //     $cr=$amount;
+        // }else{
+        //     $dr=-1*$amount;
+        //     $cr=0;
+        // }
+
         // Parse dates
         $valueDate = \DateTime::createFromFormat('ymd', $match[1]);
         $valueDate->setTime(0, 0, 0);
@@ -327,6 +409,7 @@ class Mt940
         $full_description=($descr[1])?$descr[1]:$descr[0];
         if(strlen($add_info)>1)$full_description=$add_info."\n".$full_description;
         $description_lines=explode("\n",$full_description);
+        if($type_descr!='')array_unshift($description_lines, $type_descr);
         $full_description=implode("\n",array_filter($description_lines));
 
 
